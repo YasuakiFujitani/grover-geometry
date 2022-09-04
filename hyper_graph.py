ac5c557efed105ea1087372d90ff46448d76264f
ckiumi <c.kiumi.oval17@gmail.com>
2022-09-04 14:06:24 +0900

    create ricci for new distance

--- hyper_graph.py
+++ hyper_graph.py
@@ -1,58 +1,50 @@
-import matplotlib.pyplot as plt
-import hypernetx as hnx
 from hypernetx import Hypergraph, drawing
 from networkx import fruchterman_reingold_layout as layout
-from typing import List, Tuple
 import matplotlib.pyplot as plt
-import networkx as nx
 import ot
 import numpy as np
 from pprint import pprint
+Basis = tuple[int,int]
 class HyperGraph():
+    # bases are pairs of node and edge (node, edge)
     def __init__(self, edges:list[list[int]]):
-        self.graph =  Hypergraph(edges)
-        self.basis = [(node,edge) for edge, nodes in enumerate(edges) for node in nodes ]
+        self.g =  Hypergraph(edges)
+        self.edges = edges
+        self.bases = [(node, edge) for edge, nodes in enumerate(edges) for node in nodes ]
         
-    def distance(self, basis_from:tuple[int,int], basis_to:Tuple[int,int]):
-        return 0 if basis_from == basis_to else self.graph.distance(basis_from[0], basis_to[0]) + 1
+    def distance(self, basis_from:Basis, basis_to:Basis):
+        if basis_from == basis_to:
+            return 0
+        adj_edges = [i for i in range(len(self.edges)) if basis_from[0] in self.edges[i]]
+        return min([self.g.edge_distance(str(edge), self.g.edges[str(basis_to[1])]) for edge in adj_edges]) + 1
 
-    def get_distr(self, basis:tuple[int, int]):
+    def get_distr(self, basis:Basis):
         (node, _) = basis; 
-        coined_nbrs = [basis] + list(filter(lambda x: x[0]==node and x!=basis, self.basis))      
+        coined_nbrs = [basis] + list(filter(lambda x: x[0]==node and x!=basis, self.bases))      
         deg_n = len(coined_nbrs)
         coined_distr = [2 / deg_n  - 1] + [2 / (deg_n)] * (deg_n - 1)
         nbrs = []; distr = []
         for i in range(len(coined_nbrs)):
-            shifted_nbrs = [coined_nbrs[i]] + list(filter(lambda x: x[1]==coined_nbrs[i][1] and x!=coined_nbrs[i], self.basis))      
+            shifted_nbrs = [coined_nbrs[i]] + list(filter(lambda x: x[1]==coined_nbrs[i][1] and x!=coined_nbrs[i], self.bases))      
             nbrs += shifted_nbrs
             deg_e = len(shifted_nbrs)
             distr += list(coined_distr[i] * np.array([2 / deg_e  - 1] + [2 / (deg_e)] * (deg_e - 1)))
         return distr, nbrs
 
-    def signed_measure(self, basis_from:tuple[int,int], basis_to:tuple[int,int]):
+    def signed_measure(self, basis_from:Basis, basis_to:Basis):
         x, source_nbr = self.get_distr(basis_from)
         y, target_nbr = self.get_distr(basis_to)
-        pops = [] 
-        for i in range(len(source_nbr)):    
+        for i in range(len(source_nbr)-1,-1,-1):    
                 if x[i] < 0:
-                    target_nbr.append(source_nbr[i])
-                    y.append(-x[i])
-                    pops.append(i)
-        for index in sorted(pops, reverse=True):
-            del source_nbr[index]
-            del x[index]
-        pops = []
-        for i in range(len(target_nbr)):    
+                    target_nbr.append(source_nbr.pop(i))
+                    y.append(-x.pop(i))
+        for i in range(len(target_nbr)-1,-1,-1):    
                 if y[i] < 0:
-                    source_nbr.append(target_nbr[i])
-                    x.append(-y[i])
-                    pops.append(i)
-        for index in sorted(pops, reverse=True):
-            del target_nbr[index]
-            del y[index]
+                    source_nbr.append(target_nbr.pop(i))
+                    x.append(-y.pop(i))
         return (x, source_nbr), (y, target_nbr)       
 
-    def ricci(self,basis_from:tuple[int,int], basis_to:tuple[int,int]):
+    def ricci(self,basis_from:Basis, basis_to:Basis):
         output={}
         (x, source_nbr), (y, target_nbr) = self.signed_measure(basis_from, basis_to)
         d=[[self.distance(snbr, tnbr) for tnbr in target_nbr] for snbr in source_nbr]
@@ -60,16 +52,17 @@ class HyperGraph():
         return output
     
     def arc_forward(self,source,target):
-        return [(edge[0],edge[1]) for edge in self.graph.edges() if edge[0] == target and edge[1] !=source]
+        return [(edge[0],edge[1]) for edge in self.g.edges() if edge[0] == target and edge[1] !=source]
 
     def plot(self):
-        drawing.draw(self.graph)
+        drawing.draw(self.g)
 
-edges = [[0, 1, 2], [2, 3, 4], [4, 5, 6]]
+# edges = [[0, 1], [1, 2], [2,3],[3,4]]
+node = list(range(0, 4))
+edges = [[a, b] for idx, a in enumerate(node) for b in node[idx + 1 :]]
 H = HyperGraph(edges)
-# pprint(H.basis)
-pprint(H.distance((2,1),(2,0)))
-# pprint(H.get_distr((2,1)))
-# pprint(H.ricci((2,1),(2,0)))
-H.plot()
+print(edges)
+pprint(H.ricci((1,0),(0,0)))
+pprint(H.ricci((1,0),(2,3)))
+# H.plot()
     
--- lib.py
+++ lib.py
@@ -6,64 +6,45 @@ import ot
 import numpy as np
 from pprint import pprint
 class ArcGraph():
-    graph = DiGraph()
-
     def __init__(self, edges: List[Tuple[int, int]]) -> None:
         self.graph = DiGraph(edges + [(b, a) for (a, b) in edges])
 
     def complete_graph(self, n: int) -> None:
-        node = list(map(str, range(0, n)))
+        node = list(range(0, n))
         edges = [(a, b) for idx, a in enumerate(node) for b in node[idx + 1 :]]
         self.graph = DiGraph(edges + [(b, a) for (a, b) in edges])
         
     def distance(self, arc_from:Tuple[int,int], arc_to:Tuple[int,int]):
         return 0 if arc_from == arc_to else nx.shortest_path_length(self.graph,arc_from[1], arc_to[0]) + 1
 
-    def get_distr(self, source, target): 
-        nbrs = list(filter(lambda x: x[0]==target and x[1]!=source, self.graph.edges()))        
-        return [2 / (len(nbrs) + 1)  - 1] + [2 / (len(nbrs) + 1)] * len(nbrs), [(target,source)] + nbrs
+    def get_distr(self, arc:Tuple[int,int]): 
+        nbrs = list(filter(lambda x: x[0]==arc[1] and x[1]!=arc[0], self.graph.edges()))        
+        return [2 / (len(nbrs) + 1)  - 1] + [2 / (len(nbrs) + 1)] * len(nbrs), [(arc[1],arc[0])] + nbrs
 
-    def signed_measure(self, source, target, target_forward):
-        x, source_nbr = self.get_distr(source, target)
-        y, target_nbr = self.get_distr(target, target_forward)
-        for i in range(len(source_nbr)):    
+    def signed_measure(self, source:tuple[int,int], target:tuple[int,int]):
+        x, source_nbr = self.get_distr(source)
+        y, target_nbr = self.get_distr(target)
+        for i in range(len(source_nbr)-1,-1,-1):
                 if x[i] < 0:
                     target_nbr.append(source_nbr.pop(i))
                     y.append(-x.pop(i))
-                    break
-        for i in range(len(target_nbr)):    
+        for i in range(len(target_nbr)-1,-1,-1):    
                 if y[i] < 0:
                     source_nbr.append(target_nbr.pop(i))
                     x.append(-y.pop(i))
-                    break
         return (x, source_nbr), (y, target_nbr)       
 
-    def ricci_reverse(self):
-        output={}
-        for edge in self.graph.edges():
-            (x, source_nbr), (y, target_nbr) = self.signed_measure(edge[0], edge[1], edge[0])
-            d=[[self.distance(snbr, tnbr) for tnbr in target_nbr] for snbr in source_nbr]
-            output[(edge[0],edge[1]),(edge[1],edge[0])]= 1 - ot.emd2(np.array(x), np.array(y), d) / self.distance((edge[0],edge[1]),(edge[1],edge[0])) 
-        return output
-
-    def ricci_forward(self):
-        output={}
-        for edge in self.graph.edges():
-            arc_forward = self.arc_forward(edge[0],edge[1])
-            for (_,target_forward) in arc_forward:
-                (x, source_nbr),(y, target_nbr) = self.signed_measure(edge[0],edge[1],target_forward)
-                d=[[self.distance(snbr,tnbr) for tnbr in target_nbr] for snbr in source_nbr]
-                output[(edge[0],edge[1]),(edge[1],target_forward)]= 1 - ot.emd2(np.array(x), np.array(y), d) / self.distance((edge[0],edge[1]),(edge[1],edge[0])) 
-        return output
-    
-    def arc_forward(self,source,target):
-        return [(edge[0],edge[1]) for edge in self.graph.edges() if edge[0] == target and edge[1] !=source]
+    def ricci(self, source:tuple[int,int], target:tuple[int,int]):
+        (x, source_nbr), (y, target_nbr) = self.signed_measure(source, target)
+        d=[[self.distance(snbr, tnbr) for tnbr in target_nbr] for snbr in source_nbr]
+        output = 1 - ot.emd2(np.array(x), np.array(y), d) / self.distance(source,target) 
+        return {(source,target):output}
 
     def plot(self):
         nx.draw(self.graph, with_labels=True, connectionstyle="arc3, rad = 0.1")
         plt.show()
 
 G=ArcGraph([(1,2),(2,3),(3,4),(4,5)])
-G.complete_graph(3)
-pprint(G.ricci_reverse())
-# print(G.ricci_forward())
+G.complete_graph(4)
+print(G.ricci((0,1),(1,0)))
+print(G.ricci((0,1),(1,2)))
